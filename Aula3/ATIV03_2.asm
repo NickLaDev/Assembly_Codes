1593e9ed6ff200af47f970cf1ee3a461247791b1
NickLaDev <nicolas.laredo@yahoo.com>
2024-08-27 11:30:07 -0300

    ter

---
+++ Aula3/ATIV03_2.asm
@@ -0,0 +1,30 @@
+TITLE vereficador
+.MODEL SMALL
+.STACK 100h
+.DATA 
+    MSG1 db "Digite um caracter, $"
+    NUMERO db 10,13,"Oque foi digitado eh um numero! $"
+    LETRA db 10,13,"Oque foi digitado eh uma letra! $"
+    DESCONHECIDO db 10,13,"Oque foi digitado eh desconhecido! $"
+.CODE
+
+ main proc
+
+    ;Libera o acesso a memoria
+    MOV ax,@DATA
+    mov ds,ax
+
+    ;Exibe a mensagem 1
+    mov ah,9
+    LEA dx,MSG1
+    int 21h
+
+    ;Le um caracter do teclado e salva em AL
+    mov ah,1
+    int 21h
+
+    ;Copia o caracter para bl
+    mov bl,AL
+
+    ;Comeca as comparacoes e pulos:
+    CMP bl,47
