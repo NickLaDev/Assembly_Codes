02e218b9b01dd47e7f7bd1c87c71ea9f5252f7c2
NickLaDev <nicolas.laredo@yahoo.com>
2024-12-03 13:55:24 -0300

    Ex-10

--- prova03/ex10.asm
+++ prova03/ex10.asm
@@ -2,17 +2,34 @@ title valor medio de um vetor
 .model small
 .stack 100h
 .data
-
+    vetor db 1,2,3,1,2,0
 .code
 main proc
 
-         mov ax,@data
-         mov ds,ax
+               mov   ax,@data
+               mov   ds,ax
+               mov   es,ax
+               xor   dx,dx
+    ;usar lodsb para tacar do vetor para al
 
+               lea   si,vetor
+               mov   cx,6
+               cld
 
+    calc_Media:
+               lodsb
 
-         mov ah,4ch
-         int 21h
+               add   dl,al
+
+               loop  calc_Media
+
+               
+               mov   ah,2
+               add   dl,30h
+               int   21h
+
+               mov   ah,4ch
+               int   21h
 
 main endp
 end main
